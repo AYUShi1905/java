da77715d788146723477661ff19862750e082533
AYUShi1905 <131136568+AYUShi1905@users.noreply.github.com>
2024-06-01 17:26:09 +0530

    folder change

---
+++ lab_practicals/p16_CopyContent.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class p16_CopyContent {
+    public static void main(String[] args) {
+        String sourceFilePath = "source.txt";
+        String destinationFilePath = "destination.txt";
+        try {
+        Files.copy(Paths.get(sourceFilePath), Paths.get(destinationFilePath));
+        System.out.println("File copied successfully.");
+        } catch (IOException e) {
+        System.err.println("Error occurred while copying the file.");
+        e.printStackTrace();
+        }
+        }
+}
